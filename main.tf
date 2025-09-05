d983d79e39af51bf9b933ee9881fe622c3cac6d3
Andreas Krummrich <andreas@krummrich.org>
2025-09-05 07:52:32 +0200

    add random string

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
+resource "random_string" "name" {
+  length = 4
+  upper  = false
+  lower = true
+  numeric = true
+  special = false
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name
+  name     = "${var.rg_name}-${random_string.name.result}"
   location = var.rg_location
   tags     = var.rg_tags
 }
